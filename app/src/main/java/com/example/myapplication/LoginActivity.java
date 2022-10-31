d29469abd55b951234c025441d4364fe7cbc6fdb
tudornico <Emma4575@yahoo.com>
2022-10-31 13:49:45 +0200

    login tryout

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,28 +2,88 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button goToRegisterBtn;
-
+    private TextView usernameInput;
+    private TextView passwordInput;
+    ProgressDialog pd;
+    private FirebaseAuth firebaseAuth;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+            //todo redirect to profile or some shit like that
+            //startActivity(new Intent(getApplicationContext(), Dashboard.class));
+        }
+        //start the login
         goToRegisterBtn = findViewById(R.id.goToRegister_button);
-
-        goToRegisterBtn.setOnClickListener(new View.OnClickListener() {
+        pd = new ProgressDialog(this);
+        usernameInput =  findViewById(R.id.editTextTextPersonName);
+        passwordInput = findViewById(R.id.editTextTextPassword);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                userLogin();
                 startActivity(intent);
             }
         });
+        goToRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+    public void userLogin(){
+        String email = usernameInput.getText().toString().trim();
+        String password = passwordInput.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Enter pass", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        pd.setMessage("wait");
+        pd.show();
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            finish();
+                            //todo redirect to the next activity
+                            //startActivity(new Intent(getApplicationContext(), ));
+                        }
+                    }
+                });
+    }
+
 }
