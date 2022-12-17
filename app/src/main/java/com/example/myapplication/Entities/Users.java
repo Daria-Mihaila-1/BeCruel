89f6115de8c52ca8483a0fa086665d28bdbb108b
cocojambo02 <cosmin.neamtiu@stud.ubbcluj.ro>
2022-12-17 21:41:27 +0200

    fixed firestone database set username for user and added register to firestone upon registration

---
+++ app/src/main/java/com/example/myapplication/Entities/Users.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.Entities;
+
+public class Users {
+    public String username;
+    public String password;
+    public String email;
+
+    public Users(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -12,12 +12,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.Entities.Users;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 
 public class RegisterActivity extends AppCompatActivity {
     private TextView emailInput;
@@ -27,6 +37,7 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
     private ProgressDialog progressDialog;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,19 +50,15 @@ public class RegisterActivity extends AppCompatActivity {
         emailInput = findViewById(R.id.editTextTextEmailAddress);
         usernameInput = findViewById(R.id.editTextTextPersonName2);
         passwordInput = findViewById(R.id.editTextTextPassword);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onRegister();
-            }
-        });
+        registerButton.setOnClickListener(view -> onRegister());
         progressDialog = new ProgressDialog(this);
     }
 
     private void onRegister(){
         String email = emailInput.getText().toString().trim();
-        String password = passwordInput.toString().trim();
-        String username = usernameInput.toString().trim();
+        String password = passwordInput.getText().toString().trim();
+        String username = usernameInput.getText().toString().trim();
+
         if(TextUtils.isEmpty(email))
         {
             Toast.makeText(this,"Enter Email",Toast.LENGTH_SHORT).show();
@@ -69,22 +76,36 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(this,"Enter Username",Toast.LENGTH_SHORT).show();
             return;
         }
+
+
         progressDialog.setMessage("wait");
         progressDialog.show();
         firebaseAuth.createUserWithEmailAndPassword(email,password).
-                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressDialog.dismiss();
-                        if(task.isSuccessful()){
-                            //TODO : insert into users
-                            System.out.println(email + " " + password);
-                            Toast.makeText(RegisterActivity.this,"Reg Success",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-                        }
-                        else{
-                            Toast.makeText(RegisterActivity.this,"register failed",Toast.LENGTH_SHORT).show();
-                        }
+                addOnCompleteListener(this, task -> {
+                    progressDialog.dismiss();
+                    if(task.isSuccessful()){
+                        //TODO : insert into users
+                        System.out.println(email + " " + password);
+
+                        db = FirebaseFirestore.getInstance();
+
+                        Map<String, String> user = new HashMap<>();
+
+                        user.put("email", email);
+                        user.put("username", username);
+
+
+
+                        db.collection("Users").document(email)
+                                .set(user)
+                                .addOnSuccessListener(aVoid -> System.out.println("User added"))
+                                .addOnFailureListener(e -> System.out.println("Failed to add user"));
+
+                        Toast.makeText(RegisterActivity.this,"Reg Success",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this,"register failed",Toast.LENGTH_SHORT).show();
                     }
                 });
     }
